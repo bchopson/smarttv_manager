9f46f28fa2b0cfd7720531621cdec689eced0d1c
Ben Chopson <bjamin513@gmail.com>
2016-02-21 15:15:56 -0500

    Added basic TV and Slide models.

--- homepages/admin.py
+++ homepages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tv
+
+admin.site.register(Tv)
--- homepages/models.py
+++ homepages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tv(models.Model):
+    ip_address = models.URLField(max_length=200)
+
+class Slide(models.Model):
+    tv = models.ForeignKey(Tv, on_delete=models.CASCADE)
+    url = models.URLField(max_length=200)
+    duration = models.IntegerField(default=0)
