6cb095f08815f638f1a73fa36df23f56f3c51985
Brandon James <bjames105@outlook.com>
2016-04-19 20:14:45 -0400

    Moved the TV page to the main page

--- smarttv_manager/views.py
+++ smarttv_manager/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from homepages.models import Tv
+
 
 def index(request):
+    tvs = Tv.objects.all()
     template = loader.get_template('smarttv_manager/index.html')
-    return HttpResponse(template.render(request))
+    context = {
+        'tvs': tvs,
+    }
+    return HttpResponse(template.render(context, request))
